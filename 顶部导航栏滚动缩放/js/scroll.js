59ba31a055c03f39b3d3e9d899ad48e354f5056d
Lobfly <2849208927@qq.com>
2021-05-11 15:34:30 +0800

    修改onMousewheel为onScroll

--- 顶部导航栏滚动缩放/js/scroll.js
+++ 顶部导航栏滚动缩放/js/scroll.js
@@ -1,34 +1,36 @@
 var nav = document.getElementById("nav");
 var navList = document.getElementById("nav_ul");
 var speed = 1;
-var timer1,timer2 = null;
+var timer1,
+  timer2 = null;
 function up() {
-  if(nav.offsetHeight == 100){
+  if (nav.offsetHeight == 100) {
     return;
   }
   nav.style.height = nav.offsetHeight + speed + "px"; //nav和navList的高度同时变化
   navList.style.height = nav.offsetHeight + speed + "px";
-  setTimeout(up,5);
+  setTimeout(up, 5);
 }
 function down() {
-  if(nav.offsetHeight == 80){
+  if (nav.offsetHeight == 80) {
     return;
   }
   nav.style.height = nav.offsetHeight - speed + "px"; //nav和navList的高度同时变化
   navList.style.height = nav.offsetHeight - speed + "px";
-  setTimeout(down,5);
+  setTimeout(down, 5);
 }
-document.onmousewheel = function () {
+document.onscroll = function () {
   var e = e || window.event;
-  if (document.documentElement.scrollTop < 120) {
-    //滚动条距离顶部距离小于120时放大导航栏
+
+  if (document.documentElement.scrollTop < 100) {
+    //滚动条距离顶部距离小于100时放大导航栏
     if (nav.offsetHeight == 80) {
       up();
     }
-  } else if (e.wheelDelta < 0) {
-    //向下滚动
+  } else if (document.documentElement.scrollTop >= 100) {
+    //滚动条距离顶部距离大于等于100时放大导航栏
     if (nav.offsetHeight == 100) {
-       down();
+      down();
     }
   }
 };
