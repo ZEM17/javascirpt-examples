796e3dc9717e3ab41eed1bf91c29642c2f93f526
Lobfly <2849208927@qq.com>
2021-05-11 11:18:54 +0800

    替换setInterval为setTimeout

--- 顶部导航栏滚动缩放/js/scroll.js
+++ 顶部导航栏滚动缩放/js/scroll.js
@@ -1,33 +1,34 @@
 var nav = document.getElementById("nav");
 var navList = document.getElementById("nav_ul");
 var speed = 1;
-var timer = null;
-
+var timer1,timer2 = null;
+function up() {
+  if(nav.offsetHeight == 100){
+    return;
+  }
+  nav.style.height = nav.offsetHeight + speed + "px"; //nav和navList的高度同时变化
+  navList.style.height = nav.offsetHeight + speed + "px";
+  setTimeout(up,5);
+}
+function down() {
+  if(nav.offsetHeight == 80){
+    return;
+  }
+  nav.style.height = nav.offsetHeight - speed + "px"; //nav和navList的高度同时变化
+  navList.style.height = nav.offsetHeight - speed + "px";
+  setTimeout(down,5);
+}
 document.onmousewheel = function () {
   var e = e || window.event;
   if (document.documentElement.scrollTop < 120) {
     //滚动条距离顶部距离小于120时放大导航栏
     if (nav.offsetHeight == 80) {
-      timer = setInterval(function () {
-        nav.style.height = nav.offsetHeight + speed + "px"; //nav和navList的高度同时变化
-        navList.style.height = nav.offsetHeight + speed + "px";
-        if (nav.offsetHeight == 100) {
-          clearInterval(timer);
-        }
-      }, 10);
-      //
+      up();
     }
   } else if (e.wheelDelta < 0) {
     //向下滚动
     if (nav.offsetHeight == 100) {
-      //防止设置多个定时器
-      timer = setInterval(function () {
-        nav.style.height = nav.offsetHeight - speed + "px"; //nav和navList的高度同时变化
-        navList.style.height = nav.offsetHeight - speed + "px";
-        if (nav.offsetHeight == 80) {
-          clearInterval(timer);
-        }
-      }, 10);
+       down();
     }
   }
 };
